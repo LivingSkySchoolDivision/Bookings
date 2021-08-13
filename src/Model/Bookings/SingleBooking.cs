536d862e04b952246aee846bec45a89c61c665d7
Mark Strendin <mark.strendin@lskysd.ca>
2021-08-12 20:58:03 -0600

    Actually show a calendar with some (test) events

--- src/Model/Bookings/SingleBooking.cs
+++ src/Model/Bookings/SingleBooking.cs
@@ -5,8 +5,8 @@ namespace LSSD.Bookings
     public class SingleBooking : IGUIDable
     {
         public DateTime BookingDate { get; set; }        
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DateTime StartTimeUTC { get; set; }
+        public int DurationMinutes { get; set; }
         public string ShortDescription { get; set; }
         public string Requestor { get; set; }
         public string Details { get; set; }
