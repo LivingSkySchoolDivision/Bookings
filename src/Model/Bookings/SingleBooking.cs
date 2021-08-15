bff31973ef9f0de7cc597c96d3ceaf361587ea3c
Mark Strendin <mark.strendin@gmail.com>
2021-08-15 16:07:35 -0600

    Add a bunch of styles, and a mostly working booking form It just lacks a save function, so now we'll need some repositories and services.

--- src/Model/Bookings/SingleBooking.cs
+++ src/Model/Bookings/SingleBooking.cs
@@ -5,14 +5,32 @@ namespace LSSD.Bookings
     public class SingleBooking : IGUIDable
     {
         public DateTime BookingDate { get; set; }        
-        public DateTime StartTimeUTC { get; set; }
-        public int DurationMinutes { get; set; }
         public string ShortDescription { get; set; }
         public string Requestor { get; set; }
         public string Details { get; set; }
         public Guid ResourceGUID { get; set; }        
         public Guid Id { get; set; }
+        
+        public string ActualRequestor { get; set; }
+        public DateTime CreatedUTC { get; set; }
 
+        public int StartHourUTC { get; set; } // Blazor InputNumber doesn't support Bytes, otherwise I'd use Byte here
+        public int StartMinuteUTC { get; set; } // Blazor InputNumber doesn't support Bytes, otherwise I'd use Byte here
+        public int DurationMinutes { get; set; }
+
+
+
+        public DateTime StartTimeUTC { get
+            {
+                return new DateTime(
+                    this.BookingDate.Year,
+                    this.BookingDate.Month,
+                    this.BookingDate.Day,
+                    this.StartHourUTC,
+                    this.StartMinuteUTC,
+                    0);
+            }
+        }
         public DateTime EndTimeUTC { 
             get 
             {
