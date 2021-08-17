e6358c883338f8cc45eb65b6ee84379b3bbb3bc0
Mark Strendin <mark.strendin@gmail.com>
2021-08-16 20:07:03 -0600

    Use Swagger on the API always (it's read-only, not concerned about abuse at this time)

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -58,9 +58,10 @@ namespace APILSSD.Bookings.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             
             if (HostedInContainer)
             {
