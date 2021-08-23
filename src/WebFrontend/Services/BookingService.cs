bada5b90bbc3fa8e63676dadcd1df96b2c93081b
Mark Strendin <mark.strendin@gmail.com>
2021-08-22 21:23:51 -0600

    Adjust for time zones if a timezone setting exists This is not a great way to do this if we need to support multiple timezones, but for now we don't.

--- src/WebFrontend/Services/BookingService.cs
+++ src/WebFrontend/Services/BookingService.cs
@@ -14,43 +14,120 @@ namespace WebFrontend
     public class BookingService
     {
         private readonly IRepository<SingleBooking> _repository;
-        private readonly IConfiguration configuration;
+        private readonly IConfiguration _configuration;
+
+        private TimeZoneInfo AdjustedTimeZone;
+        private bool AdjustTimeZone = false;
 
 
         public BookingService(IRepository<SingleBooking> Repository, IConfiguration Configuration)
         {
             this._repository = Repository;
-            configuration = Configuration;
-        }
-        public void Insert(SingleBooking obj) 
-        {
-            // Adjust time zones to UTC
-            // Adjust booking date (if applicable)
-            // Adjust booking hour and minute
+            _configuration = Configuration;
 
-            _repository.Insert(obj);
+            if(!string.IsNullOrEmpty(_configuration["Settings:TimeZone"])) {
+            try {
+                TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById(_configuration["Settings:TimeZone"]);
+                AdjustedTimeZone = timeZone;
+                AdjustTimeZone = true;
+            }
+            catch {}
         }
-        public void Update(SingleBooking obj) 
+        }
+        public void Insert(SingleBooking obj)
         {
-            // Adjust time zones to UTC
+            SingleBooking newObj = obj;
 
-            _repository.Update(obj);
+            if(AdjustTimeZone) {
+                newObj = AdjustTimeZoneToUTC(obj, AdjustedTimeZone);
+            }
+
+            _repository.Insert(newObj);
+        }
+        public void Update(SingleBooking obj)
+        {
+            SingleBooking newObj = obj;
+
+            if(AdjustTimeZone) {
+                newObj = AdjustTimeZoneToUTC(obj, AdjustedTimeZone);
+            }
+
+            _repository.Update(newObj);
         }
 
         public SingleBooking Get(string id)
         {
             // Adjust time zones to UTC
+            SingleBooking UTCBooking =  _repository.GetById(id);
 
-            return _repository.GetById(id);
+            if (AdjustTimeZone)  {
+                UTCBooking = AdjustTimeZoneFromUTC(UTCBooking, AdjustedTimeZone);
+            }
+
+            return UTCBooking;
         }
 
         public IList<SingleBooking> Find(Expression<Func<SingleBooking, bool>> predicate)
         {
             // Adjust time zones to UTC
+            IList<SingleBooking> UTCResults = _repository.Find(predicate);
 
-            return _repository.Find(predicate);
+            if (AdjustTimeZone) {
+                IList<SingleBooking> _convertedResults = new List<SingleBooking>();
+                foreach(SingleBooking booking in UTCResults) {
+                    _convertedResults.Add(AdjustTimeZoneFromUTC(booking, AdjustedTimeZone));
+                }
+                return _convertedResults;
+            } else {
+                return UTCResults;
+            }
         }
-        
+
+        private static SingleBooking AdjustTimeZoneToUTC(SingleBooking booking, TimeZoneInfo TimeZone) {
+            // Adjust time zones to UTC
+
+            // Create a date in the user's timezone
+            DateTime nonUTCStartDate = new DateTime(
+                booking.BookingDate.Year,
+                booking.BookingDate.Month,
+                booking.BookingDate.Day,
+                booking.StartHourUTC,
+                booking.StartMinuteUTC,
+                0);
+
+            // Adjust that date to the server timezone and extract parts
+            DateTime UTCStartDateWithTime = TimeZoneInfo.ConvertTimeToUtc(nonUTCStartDate, TimeZone);
+
+            booking.BookingDate = UTCStartDateWithTime.Date;
+            booking.StartHourUTC = UTCStartDateWithTime.Hour;
+            booking.StartMinuteUTC = UTCStartDateWithTime.Minute;
+
+            return booking;
+        }
+
+        private static SingleBooking AdjustTimeZoneFromUTC(SingleBooking booking, TimeZoneInfo TimeZone) {
+            // Adjust time zones to UTC
+
+            // Create a date in the user's timezone
+            DateTime nonUTCStartDate = new DateTime(
+                booking.BookingDate.Year,
+                booking.BookingDate.Month,
+                booking.BookingDate.Day,
+                booking.StartHourUTC,
+                booking.StartMinuteUTC,
+                0);
+
+            // Adjust that date to the server timezone and extract parts
+            DateTime UTCStartDateWithTime = TimeZoneInfo.ConvertTimeFromUtc(nonUTCStartDate, TimeZone);
+
+            booking.BookingDate = UTCStartDateWithTime.Date;
+            booking.StartHourUTC = UTCStartDateWithTime.Hour;
+            booking.StartMinuteUTC = UTCStartDateWithTime.Minute;
+
+            return booking;
+        }
+
+
 
     }
 }
